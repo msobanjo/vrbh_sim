e2a81fa8e059851be8ab366090a8d0bc98d31535
GeorgeLenton <georgelenton@gmail.com>
2016-02-03 18:36:40 +0000

    Created basic animation

--- simulation/node-init.py
+++ simulation/node-init.py
@@ -6,66 +6,6 @@ from Node import Node
 
 ###############################################################################
 
-class Node:
-    def __init__(self, x1, y1,
-                 side_len,
-                 canvas_in,
-                 weight=1,
-                 price=5,
-                 colour="#555",
-                 outline="#666",
-                 robot_colour = "#f27"
-                 ):
-        """
-        Represents one square on screen.
-        """
-
-        x2 = x1 + side_len
-        y2 = y1 + side_len
-
-        self.x1 = x1
-        self.y1 = y1
-        self.x2 = x2
-        self.y2 = y2
-        self.colour = colour
-        self.robot_colour = robot_colour
-        self.canv = canvas_in
-        self.outline = outline
-        self.weight = weight
-        self.value = price
-        self.am_robot = False
-
-    def display(self):
-        """display the vertice on screen"""
-        self.canv.create_rectangle(self.x1,
-                                self.y1,
-                                self.x2,
-                                self.y2,
-                                fill= self.colour,
-                                outline=self.outline)
-
-    def set_robot(self):
-        """Currently this will colour a node to whatever is set to be the robot colour
-        value.
-        """
-        self.am_robot = True
-        self.set_colour(self.robot_colour)
-
-    def set_colour(self, c):
-        """
-        Assign a given colour to a node
-        """
-        self.colour = c
-
-    def __str__(self):
-        """Print off some basic info about the queried node coordinates
-        """
-        info = "x1 = {}, y1 = {},\nx2 = {}, y2 = {}\n".format(
-            self.x1, self.y1, self.x2, self.y2)
-        return info
-
-###############################################################################
-
 root = Tk()
 
 # variables to use for node matrix size and the square size of the nodes.
@@ -122,20 +62,20 @@ set_objs = [random.randint(1,MATRIX_TOTAL) for x in range(number_of_test_objects
 # this function is what's called by the root.after() method. Note that it has
 # to have root.after() within itself, and that the method takes
 
-def test_animate(s=877, n=0):
+def main_animate(s=877, n=0):
     s += n
     set_objs = [random.randint(1,MATRIX_TOTAL) for x in range(number_of_test_objects)]
-    print("runrunrunr")
     for index, node in enumerate(node_dict):
         if index in set_objs:
             node_dict[node].set_colour("#4f1")
         if index == s:
             node_dict[node].set_robot()
         node_dict[node].display()
-    root.after(SCREEN_REFRESH, test_animate)
+    root.after(SCREEN_REFRESH, main_animate)
 
-root.after(SCREEN_REFRESH, test_animate)
-test_animate()
+
+root.after(SCREEN_REFRESH, main_animate)
+main_animate()
 
 root.mainloop()
 root.destroy()
