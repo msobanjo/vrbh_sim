5ee411f2061c36c57c8a9ba82a539396750ed348
Michael <msobanjo@hotmail.com>
2016-02-05 12:02:40 +0000

    Changed to 2.7

--- interface testing/basic interface 2.7 .py
+++ interface testing/basic interface 2.7 .py
@@ -1,13 +1,14 @@
 import sys
-from tkinter import *
-from tkinter import ttk
 
+from Tkinter import *
+import ttk
 
 class Main:
+
     def Done():
             mtext = text.get()
             print(mtext)
-    
+
 
     def __init__(self, root):
         self.text = StringVar()
@@ -20,30 +21,25 @@ class Main:
 
         self.button = Button(root,text="ok", command = self.Done)
         self.button.place(x=300,y=200)
-        
-         
+
+
 
         self.textbox = Entry(root,textvariable = self.text)
         self.textbox.place(x=150, y=50)
 
         self.textbox1 = Entry(root)
         self.textbox1.place(x=150, y=70)
-        self.text2 =StringVar() 
+        self.text2 = StringVar()
 
-        self.combo = ttk.Combobox(self.root, textvarible = self.text2).pack()
+        self.combo = ttk.Combobox(self.root, textvariable = self.text).pack()
 
-    def Done():
-         mtext = text.get()
+    def Done(self):
+         mtext = self.text.get()
          print(mtext)
-    
-
-
-
 
 
 
 mGui = Tk()
 gui = Main(mGui)
 mGui.geometry('450x450+200+200')
-
 mGui.mainloop()
