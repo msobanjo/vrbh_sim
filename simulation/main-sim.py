34e9e3a08db0da7433c6052c11ebf723ad7b772f
George Lenton (cb) <georgelenton@gmail.com>
2016-02-18 09:52:24 +0000

    Tidy up file names and delete unused
    There are some files kicking about that aren't used and the testing is
    being used as the main so just rename it to main and have done with it.

--- simulation/main-sim.py
+++ simulation/main-sim.py
@@ -2,15 +2,10 @@ from Tkinter import *
 import math
 import random
 import time
-# used to convert the 2D array into a 1D array
 from itertools import chain
 
 from Node import Node
 from AllGraph import Graph
-
-# from AllGraph import Graph
-
-print("939495")
 
 ###############################################################################
 
@@ -18,9 +13,10 @@ print("939495")
 root = Tk()
 
 # INIT VARIABLES
-NODE_NUM_W = 40
-NODE_NUM_H = 40
-NODE_SIDE_LEN = 25
+NODE_NUM_W = 50
+NODE_NUM_H = 50
+NODE_SIDE_LEN = 15
+
 MATRIX_TOTAL_NODE_AMOUNT = NODE_NUM_H * NODE_NUM_W # 3x3 = 9
 MATRIX_WIDTH = NODE_NUM_W * NODE_SIDE_LEN          # eg 3x3 matrix with NODE_SIZE of 2 = 6 wide
 MATRIX_HEIGHT = NODE_NUM_H * NODE_SIDE_LEN         # eg 3x3 with NODE_SIZE 2 = 6 high
@@ -29,7 +25,8 @@ NODE_SIZE = NODE_SIDE_LEN
 CANVAS_BACKGROUND_COLOUR = 'white'
 # Think frames per seconds, just a milliseconds value of how often to refresh
 # the page
-SCREEN_REFRESH = 50
+SCREEN_REFRESH = 100
+
 
 # Create the canvas with given sizes
 canvas = Canvas(root,
@@ -40,157 +37,223 @@ canvas.pack()
 
 ###############################################################################
 
+fixed_robot = 620
+
+# TODO: Pass in a tuple instead of a fixed value. It makes no sense for the value to
+# just be one term for a 2D list, instead the Graph should receive a tuple to access the
+# node positions.
+# this should be roughly the center, 20th col in 20th row.
+
+robot_tuple = (20, 20)
+
+new_seeker_list = [fixed_robot]
 test_graph = Graph(NODE_NUM_H, NODE_NUM_W, NODE_SIZE, canvas)
 
-
-print("length = {}".format(len(test_graph.matrix)))
-print("total length = {}".format((test_graph.node_number)))
-
-def create_node_matrix(
-        NODE_NUM_H,
-        NODE_NUM_W,
-        NODE_SIZE):
-    """Takes input of the number of nodes in height and width, and the NODE_SIZE of the
-    nodes.
-
-    Creates and returns a 2D array.
-    """
-    node_list = []
-    for j in range(NODE_NUM_H): # Create each row
-        node_list.append([])
-        for i in range(NODE_NUM_W): # Create nodes in each row
-            row = j*NODE_SIZE
-            col = i*NODE_SIZE
-            # node_list[-1].append(Node(col, row, NODE_SIZE)) # Last row, add node
-            node_list[-1].append(Node(col, row, NODE_SIZE, canvas)) # Last row, add node
-    return node_list
-
-def robot_square(event):
-    """
-    Test function - Defines the clicked node as the robot from mouse selection
-    """
-    robot = node_list[event.y//NODE_SIZE][event.x//NODE_SIZE] # Get the correct node
-    robot.set_robot() # Set the node as a robot
-    robot.display()
-    canvas.unbind('<Button-1>')
-    robot.pass_the_butter() # Gets butter
-
-##########################
-
-# CREATE A FIXED ROBOT NODE
-# CREATE A FIXED OBJECT NODE
-# DRAW A LINE BETWEEN THE TWO NODES
-fixed_robot = 100
-fixed_item = 414
-
-line_nodes = []
-
-#########################
-
-def draw_line(a, b, canvas):
-    """
-    draw a line between the nodes a and b
-
-    This should be implemented on a graph structure though
-    """
-    canvas.create_line(a.mx,
-                       a.my,
-                       b.mx,
-                       b.my,
-                       fill="#7AB85C",
-                       width="6",
-                       smooth=1
-                       )
-
-bot = "test"
-it = "test"
-
-# render all nodes once then update within the loop nodes as necessary
 test_graph.render()
+test_graph.place_robot(robot_tuple)
 
-timer = 0
+###############################################################################
+
+def wait(n):
+    """
+    Used while debugging - just causes the program to wait for n seconds
+    """
+    print("Freeze for {}".format(n))
+    for i in range(n):
+        print("...")
+        time.sleep(1)
+
+def reset_values(t, b, r):
+    """Takes input of top and bottom tuples for the search graph and resets them
+    to the robot position.
+
+    """
+    t = (r[0], r[1])
+    b = (r[0], r[1])
+    return t, b
+
+def calc_tb_distance(t, b):
+    """Input of tuples t[op] and b[ottom] nodes, returns half the distance between
+    the two
+
+    """
+    difference_top_bottom = abs(t[1] - b[1]) / 2
+    return difference_top_bottom
+
+def get_reset_edges(t, b):
+    """Resetting is a bit different to the seeker edges - as ALL nodes in the
+    'diamond' need to be reset here, wheras for the seeker (get_edges()) it's
+    just the external nodes which should be set.
+    """
+    pass
+
+def get_edges(t, b, d):
+    """Input of t[op], b[ottom] tuples and the d[ifference] from them to the Robot
+
+    This will return a set() of the values that should be tested.
+
+    I'm leaving print statements in here so that you can run if you choose to
+    in order to see how it's functioning
+
+    """
+    edges = set()
+
+    for n in range(d):
+
+        n = n  + 1
+
+        topLeft = (t[0] - n, t[1] + n)
+        edges.add((topLeft))
+
+        bottomLeft = (b[0] - n, b[1] - n)
+        edges.add(bottomLeft)
+
+        topRight = (t[0] + n, t[1] + n)
+        edges.add((topRight))
+
+        bottomRight = (b[0] + n, b[1] - n)
+        edges.add((bottomRight))
+
+        print('\n')
+    return edges
+
+def get_nodes_to_reset(t, b, m):
+    """
+
+    INPUT
+    -----
+
+    t[op] > tuple > the top node of search
+
+    b[ottom] > tuple > bottom node of search
+
+    m[atrix] > 2D list > 2D list of node objects, this is the graph.
+
+    RETURN
+    ------
+
+    Returns a set() which has all of the node object index positions from the
+    search top to bottom
+
+    EXAMPLE
+    -------
+
+    m[atrix] to use for values, which will be structured as (for a 3x3 matrix
+    size where n represents a node object)
+
+    m = [
+        [n, n, n],
+        [n, n, n],
+        [n, n, n]]
+
+    Here Top and Bottom nodes represent the node on the top or the bottom of
+    the search as it's viewed from the screen.
+
+    EXAMPLE SEARCH TOP / BOTTOM
+
+    here [2,0] would be the top and [2,4] would be the bottom -
+
+                        (2, 0)
+                (1, 1), (2, 1), (3, 1)
+        (0, 2), (1, 2), (2, 2), (3, 2), (4, 2)
+                (1, 3), (2, 3), (3, 3)
+                        (2, 4)
+
+    """
+
+    # TODO: Changing 'alt' value alters the amount of 'seeker buffers' that are
+    # on the edge of the search graph
+    alt = 1
+    t = (t[0] , t[1] + alt)
+    b = (b[0], b[1] - alt)
+
+    # need half the distance between the nodes + 1 because it needs to iterate
+    # from the top node to the center position and from the bottom node to the
+    # center position
+
+    d2 = (abs(t[1] - b[1]) / 2) + 1
+
+    s = set()
+
+    # Iterate from the top to the center and from the bottom to the center.
+    for i in range(d2):
+
+        # i rows away from either the top or the bottom node
+        top_row    = t[1] + i
+        bottom_row = b[1] - i
+
+        # values that set the most left and most right nodes for each
+        # iteration. For example on the Example Matrix in the doc string for
+        # the second iteration (1,1) would be top left and (3,1) would be top
+        # right.
+        topLeft     = t[0] - i
+        topRight    = t[0] + i
+        bottomLeft  = b[0] - i
+        bottomRight = b[0] + i
+
+        # Once the top left and right nodes have been found everything
+        # inbetween them can be got through list slicing
+        top_list    = (m[top_row][topLeft:topRight+1])
+        bottom_list = (m[bottom_row][bottomLeft:bottomRight+1])
+
+        # s.update() is a method to add list values to sets. bit more info here
+        # - https://docs.python.org/2/library/sets.html#set-objects
+        s.update(top_list)
+        s.update(bottom_list)
+
+    return s
+
+
+###############################################################################
+
+TopEdge = (robot_tuple[0], robot_tuple[1])
+BottomEdge = (robot_tuple[0], robot_tuple[1])
+edges = set()
 
 def main_animate():
-    """This is currently the main section that's iterated over for the animation
-    """
-    timer += 1
 
-    robot_is_set = False
-    item_is_set = False
-    change_node_colour = random.randint(1, MATRIX_TOTAL_NODE_AMOUNT)
+    # TODO: globals urgh - shouldn't need these
+    global TopEdge
+    global BottomEdge
+    global edges
 
-    # Count is used at the moment to check which if the node is the test robot
-    # or item
-    count = 0
-    global timer
-    global fixed_robot
+    # this should be all of the edges from the previous run, which
+    # (after being searched) should now be set to sought
+    try:
+        for e in edges:
+            test_graph.set_sought(e)
+    except UnboundLocalError as firstRun:
+        # TODO: This will kick up an error on the first pass so just catch it
+        # for now
+        pass
 
-    for index_r, a_row in enumerate(test_graph.matrix):
-        prev_inc = 0
-        previous_node = a_row
-        for index_b, a_node in enumerate(a_row):
-            timer += 1
+    edges = set()
 
-            if count == fixed_robot:
+    TopEdge = (TopEdge[0], TopEdge[1] - 1)
+    BottomEdge = (BottomEdge[0], BottomEdge[1] + 1)
+    dist = calc_tb_distance(TopEdge, BottomEdge)
 
-                # increment the current node to change to searching nodes
-                if timer % 2 == 0:
-                    fixed_robot += 1
-                    timer = 0
+    edges = get_edges(TopEdge, BottomEdge, dist)
 
-                previous_node[prev_inc].set_colour("#000")
-                a_node.set_robot()
-                bot = a_node
-                robot_is_set = True
-                bot.display()
+    edges.add(TopEdge)
+    edges.add(BottomEdge)
 
-            if count == fixed_item:
-                a_node.set_colour('#fff')
-                line_nodes.append(a_node)
-                item_is_set = True
-                it = a_node
-                it.display()
+    if dist >= 8:
+        # clear the search pattern
+        reset_nodes = get_nodes_to_reset(TopEdge, BottomEdge, test_graph.matrix)
+        test_graph.reset_nodes(reset_nodes)
+        TopEdge, BottomEdge =  reset_values(TopEdge, BottomEdge, robot_tuple)
+        edges = set()
+    else:
+        # create the next line of seekers
+        for e in edges:
+            test_graph.set_seeker(e)
 
-            count += 1
-            prev_inc += 1
 
     root.after(SCREEN_REFRESH, main_animate)
 
 ###############################################################################
 
-# INIT THE MATRIX WITH NODES
-# --------------------------
-
-# Makes a 2D matrix of nodes which can be accessed through x and y co-ordinates
-node_list = create_node_matrix(NODE_NUM_H, NODE_NUM_W, NODE_SIZE)
-
-###############################################################################
-
-# CREATE EVENT LISTENER
-# ---------------------
-
-# When there is a left click, this runs robot_square function
-canvas.bind('<Button-1>', lambda event: robot_square(event))
-
-###############################################################################
-
-# CREATE SOME PURELY RANDOM 'OBJECTS'
-# -----------------------------------
-
-# this are just used for testing at the moment
-set_objs = [random.randint(0,MATRIX_TOTAL_NODE_AMOUNT) for x in range(9)]
-
-###############################################################################
-
-# DISPLAY THE NODES ON CANVAS
-# ---------------------------
-
-# Unpacks the 2D list into a list
-unpacked_list = list(chain.from_iterable(node_list))
-
-###############################################################################
-
 root.after(SCREEN_REFRESH, main_animate)
-
 root.mainloop()
 root.destroy()
