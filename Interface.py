a43e23e0c1885299e13e4906191ed63235377ca7
Michael <msobanjo@hotmail.com>
2016-03-08 12:08:43 +0000

    Changes sizes

--- Interface.py
+++ Interface.py
@@ -21,23 +21,23 @@ class Interface():
 
         self.labelType = Label(root,text="Type")
         self.labelType.configure(background='white')
-        self.labelType.place(x=100,y=70)
+        self.labelType.place(x=100,y=80)
 
         self.labelTime = Label(root,text="Time")
         self.labelTime.configure(background='white')
-        self.labelTime.place(x=100,y=110)
+        self.labelTime.place(x=100,y=160)
 
         self.labelSearching = Label(root,text="Searching")
         self.labelSearching.configure(background='white')
-        self.labelSearching.place(x=60,y=150)
+        self.labelSearching.place(x=60,y=240)
 
         self.labelSorting = Label(root,text="Sorting")
         self.labelSorting.configure(background='white')
-        self.labelSorting.place(x=80,y=190)
+        self.labelSorting.place(x=80,y=320)
 
         self.lableRobotColour = Label(root,text= "Robots colour")
         self.lableRobotColour.configure(background='white')
-        self.lableRobotColour.place(x=30,y=300)
+        self.lableRobotColour.place(x=185,y=400)
 
 
     def okButton(self):
@@ -65,30 +65,30 @@ class Interface():
           ''' displays the comboboxes on the main page'''
           self.comboType = ttk.Combobox(self.root)
           self.comboType.bind(self.comboType)
-          self.comboType.place(x=190,y=80)
+          self.comboType.place(x=190,y=90)
           #Place holder values atm
           self.comboType.config(values = ("a","b","c"))
           #self.text.get()
 
           self.textTime = StringVar()
           self.comboTime = ttk.Combobox(self.root, textvariable = self.textTime)
-          self.comboTime.place(x=190,y=120)
+          self.comboTime.place(x=190,y=170)
           self.comboTime.config(values = ("a","b","c"))
 
           self.textSearching = StringVar()
           self.comboSearching = ttk.Combobox(self.root, textvariable = self.textSearching)
-          self.comboSearching.place(x=190,y=160)
+          self.comboSearching.place(x=190,y=250)
           self.comboSearching.config(values = ("a","b","c"))
 
           self.textSorting = StringVar()
           self.comboSorting = ttk.Combobox(self.root, textvariable = self.textSorting)
-          self.comboSorting.place(x=190,y=200)
+          self.comboSorting.place(x=190,y=330)
           self.comboSorting.config(values = ("a","b","c"))
 
     def buttons(self):
         ''' displays the button on the main page'''
         self.buttonOk = Button(self.root,text="Ok", command = self.okButton)
-        self.buttonOk.place(x=350,y=350)
+        self.buttonOk.place(x=600,y=600)
            
            
 
@@ -97,9 +97,10 @@ class Interface():
         self.sliderColour = Scale(root, orient =HORIZONTAL,length=200,
           width=15,sliderlength=15,
           from_=0,to=4095,
+          highlightthickness=0,
           command=self.print_value)
        
-        self.sliderColour.place(x=100,y=380)
+        self.sliderColour.place(x=270,y=500)
         self.sliderColour.configure(background='white')
     def done(self):
         ''' close the main page and opens up a new one'''
@@ -133,11 +134,11 @@ class Info(Interface):
     def okButton1(self):
         
         self.buttonOk = Button(self.root,text="Ok",command = self.close)
-        self.buttonOk.place(x=350,y=350)
+        self.buttonOk.place(x=650,y=600)
 
     def showMeAgainButton(self):
         self.buttonShowAgain = Button(self.root,text="Don't show me again",command = self.files)
-        self.buttonShowAgain.place(x=50 , y=350)
+        self.buttonShowAgain.place(x=50 , y=600)
     def close(self):
         self.buttonShowAgain.place_forget()
         self.buttonOk.place_forget()
@@ -154,7 +155,7 @@ class Info(Interface):
          gui.slider()
          canvas.pack(padx=10,pady=240)     
     def files(self):
-        f = open('H:\ALL semester 2\dont show me.txt', 'r+')
+        f = open('C:\Users\OWNER\ALL.git\dont show me.txt', 'r+')
         f.write("true")
         f.close
         
@@ -179,12 +180,12 @@ gui = Info(root)
 
 root.configure(background='white')
 font.nametofont('TkDefaultFont').configure(size=15)
-canvas = Canvas(root, width=100, height=90)
-rectangleColour=canvas.create_rectangle(3,7,3+100,7+100,fill=Interface.value)
+canvas = Canvas(root, width=100, height=450,highlightthickness=0)
+rectangleColour=canvas.create_rectangle(3,110,3+100,110+100,fill=Interface.value)
 canvas.configure(background='white')
 
-root.geometry('450x600+200+200')
-f = open('H:\ALL semester 2\dont show me.txt', 'r+')
+root.geometry('750x750')
+f = open('C:\Users\OWNER\ALL.git\dont show me.txt', 'r+')
     
 if f.read() == "true":
     gui.close2()
